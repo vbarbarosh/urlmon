f0db4531135212bcce5c40ea0d1e7a8eb955c60f
Vladimir Barbaros <vladimir.b@bannernow.com>
2023-12-03 10:28:19 +0200

    page-about: add some dev info

--- resources/js/components/page-about.vue
+++ resources/js/components/page-about.vue
@@ -1,5 +1,26 @@
 <template>
     <div>
-        About
+        <pre>
+GET <a href="/api/v1/artifacts.json" target="_blank">/api/v1/artifacts.json</a>
+GET <a href="/api/v1/artifacts/xxx" target="_blank">/api/v1/artifacts/{artifact_uid}</a>
+DELETE <a href="/api/v1/artifacts/xxx" target="_blank">/api/v1/artifacts/{artifact_uid}</a>
+
+GET <a href="/api/v1/promises.json" target="_blank">/api/v1/promises.json</a>
+GET <a href="/api/v1/promises/xxx" target="_blank">/api/v1/promises/{promise_uid}</a>
+DELETE <a href="/api/v1/promises/xxx" target="_blank">/api/v1/promises/{promise_uid}</a>
+
+GET <a href="/api/v1/parsers.json" target="_blank">/api/v1/parsers.json</a>
+GET <a href="/api/v1/parsers/xxx" target="_blank">/api/v1/parsers/{parser_uid}</a>
+POST <a href="/api/v1/parsers" target="_blank">/api/v1/parsers</a>
+PATCH <a href="/api/v1/parsers/xxx" target="_blank">/api/v1/parsers/{parser_uid}</a>
+DELETE <a href="/api/v1/parsers/xxx" target="_blank">/api/v1/parsers/{parser_uid}</a>
+
+GET <a href="/api/v1/urls.json" target="_blank">/api/v1/urls.json</a>
+GET <a href="/api/v1/urls/xxx" target="_blank">/api/v1/urls/{url_uid}</a>
+POST <a href="/api/v1/urls" target="_blank">/api/v1/urls</a>
+POST <a href="/api/v1/urls/xxx/parse" target="_blank">/api/v1/urls/{url_uid}/parse</a>
+PATCH <a href="/api/v1/urls/xxx" target="_blank">/api/v1/urls/{url_uid}</a>
+DELETE <a href="/api/v1/urls/xxx" target="_blank">/api/v1/urls/{url_uid}</a>
+</pre>
     </div>
 </template>
